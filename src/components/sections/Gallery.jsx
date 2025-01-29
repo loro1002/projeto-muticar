55c2403ff7a193fe94fc1adf67d8709f00c0b6aa
loro1002 <jaum775@gmail.com>
2025-01-29 15:46:30 -0300

    quemsomos teste

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -38,7 +38,7 @@ const Gallery = () => {
   };
 
   return (
-    <section className="bg-gray-50 py-16" >
+    <section className="bg-gray-50 py-16" id='galeria'>
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
           <h2 className="text-4xl font-bold text-gray-800">
